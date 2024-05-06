34c7d2083e542cf4fe05db08ee7cc194b8f0cff2
Danish <dan71ish@gmail.com>
2024-05-07 01:35:12 +0530

    registration ui form component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import closeBtn from "../assets/closeBtn.png";
+import RegistrationForm from "../components/RegistrationForm";
 
 const Register = () => {
   return (
     <>
-      <div className="h-screen bg-white text-black w-full px-[2vw] py-2 md:py-4">
+      <div className="overflow-hidden h-screen bg-white text-black w-full px-[2vw] py-2 md:py-4">
         <nav className="flex justify-between items-center p-2 px-4 rounded-full">
           <Link to="/">
             <img src="./logo.png" alt="Brunel" />
@@ -13,6 +14,10 @@ const Register = () => {
             <img src={closeBtn} alt="Brunel" />
           </Link>
         </nav>
+
+        <div className="w-full h-full flex justify-center mt-28">
+          <RegistrationForm />
+        </div>
       </div>
     </>
   );
